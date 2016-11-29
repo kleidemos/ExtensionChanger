631deb005396d47e0c1adfd0679dcffa36297e57
kleidemos <arche-kunktator@yandex.ru>
2016-11-30 01:57:32 +0500

    Добавил модуль выполняющий основную работу.

---
+++ ExtensionChanger/Changer.fs
@@ -0,0 +1,7 @@
+﻿module Changer
+
+open System.IO
+
+let setExtension ext (directory:DirectoryInfo) =
+    directory.GetFiles()
+    |> Seq.iter (fun p -> File.Move(p.FullName, Path.ChangeExtension(p.FullName, ext)))
