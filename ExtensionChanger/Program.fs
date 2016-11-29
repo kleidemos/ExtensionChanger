4013998de4cbd01e70c16794fcb3c2833a9e0062
kleidemos <arche-kunktator@yandex.ru>
2016-11-30 02:18:00 +0500

    Коммит без -m.

--- ExtensionChanger/Program.fs
+++ ExtensionChanger/Program.fs
@@ -1,7 +1,10 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.org
 // Дополнительную справку см. в проекте "Учебник по F#".
+open System.IO
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // возвращение целочисленного кода выхода
+    @"C:\Assets"
+    |> DirectoryInfo
+    |> Changer.setExtension "png"
+    0
